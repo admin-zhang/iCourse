9800a0662b5b8b8af22c90255341ba1282d92c80
admin-zhang <764018641@qq.com>
2024-04-15 16:30:45 +0800

    [修改]管理员模块启动类修改

--- icourse-admin/src/main/java/club/icoders/icourse/ICourseAdminApplication.java
+++ icourse-admin/src/main/java/club/icoders/icourse/ICourseAdminApplication.java
@@ -1,5 +1,6 @@
-package dlub.icoders.icourse;
+package club.icoders.icourse;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -14,6 +15,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
 @SpringBootApplication
+@MapperScan("club.icoders.icourse.mapper")
 public class ICourseAdminApplication {
     public static void main(String[] args) {
         SpringApplication.run(ICourseAdminApplication.class, args);
