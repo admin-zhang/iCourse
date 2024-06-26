5920ee72a47bfeed16f109d8f3fd1781256f1a1c
admin-zhang <764018641@qq.com>
2024-06-26 21:27:23 +0800

    [修改]新增validation依赖和全局异常处理修改

--- icourse-common/src/main/java/club/icoders/icourse/common/exception/GlobalExceptionHandler.java
+++ icourse-common/src/main/java/club/icoders/icourse/common/exception/GlobalExceptionHandler.java
@@ -3,11 +3,13 @@ package club.icoders.icourse.common.exception;
 import club.icoders.icourse.common.api.CommonResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.validation.BindException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.ConstraintViolationException;
 
 /**
  * @ClassName GlobalExceptionHandler.java
@@ -26,6 +28,20 @@ public class GlobalExceptionHandler {
     public CommonResult<Object> icourseExceptionHandler(HttpServletResponse response, ICourseException e) {
         logger.info("发生业务异常，原因是：{}", e.getMessage());
         return CommonResult.exception(e.getErrCode(), e.getErrMessage());
+    }
+
+    @ResponseBody
+    @ExceptionHandler(value = BindException.class)
+    public CommonResult<Object> bindExceptionHandler(HttpServletResponse response, BindException e) {
+        logger.error("valid校验参数时发生异常，原因是：{}", e.getMessage());
+        return CommonResult.exception("441",e.getBindingResult().getAllErrors().get(0).getDefaultMessage());
+    }
+
+    @ResponseBody
+    @ExceptionHandler(value = ConstraintViolationException.class)
+    public CommonResult<Object> bindExceptionHandler(HttpServletResponse response, ConstraintViolationException e) {
+        logger.error("valid校验参数时发生异常，原因是：{}", e.getMessage());
+        return CommonResult.exception("441",e.getLocalizedMessage().split(":")[1].trim());
     }
 
 }
